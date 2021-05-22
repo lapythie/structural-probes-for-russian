ad90c0dd13994f00d0d0354f9b2946e02376c054
lapythie <frbrmyau@gmail.com>
2021-05-22 14:18:43 +0300

    add dataset class

--- utils/data.py
+++ utils/data.py
@@ -15,3 +15,141 @@ def random_lstm(args):
     for p in lstm.parameters():
         p.requires_grad = False
     return lstm.to(args["device"])
+
+class ProbingDataset(torch.utils.data.Dataset):
+
+    def __init__(self, args, path_to_conllu, cached_labels):
+        self.args=args
+        self.path_to_conllu = path_to_conllu
+        self.cached_labels = cached_labels
+        self.path_to_embeddings = args["featurizer"]["path_to_embeddings"]
+        self.batch_size = args["probe_training"]["batch_size"]
+        self.featurizer_type = args["featurizer"]["featurizer_type"]
+        self.split_ = [sp for sp in {"dev", "test", "train"} if sp in self.path_to_conllu][0]
+        self.Sentence = namedtuple(typename="Sentence", field_names=["tokens", "length", "tree", "embeddings"])
+        self.trees, self.roots, self.sents, self.lengths  = self.trees_sents_roots_lengths()
+        self.layer = args["featurizer"]["layer"]
+        self.path_to_tokenizer = args["featurizer"]["path_to_tokenizer"]
+        if self.path_to_tokenizer:
+            self.data_file = h5py.File(self.path_to_embeddings, mode="r")
+            self.tokenizer = BertTokenizer.from_pretrained(self.path_to_tokenizer)
+            self.subtokens, self.spans = self.wordpiece_tokenize()
+        else:
+            self.projection = args["featurizer"]["projection"]
+            self.data_file = h5py.File(f"{self.path_to_embeddings}/elmo-{self.split_}.hdf5", mode="r")
+            if args["featurizer"]["decay"]:
+                self.length_to_decay, self.length_to_norm = self.compute_decays_and_norms()
+
+    def read_embeddings(self, index):
+        if self.path_to_tokenizer:
+            sentence_embeddings = []
+            dset = self.data_file[f"{self.split_}/bert/{self.layer}/{index}"][...][0]
+            for span in self.spans[index]:
+                token_embeddings = dset[slice(*span)]
+                averaged_token_embedding = token_embeddings.mean(axis=0)
+                sentence_embeddings.append(averaged_token_embedding)
+            sentence_embeddings = np.vstack(sentence_embeddings)
+        else: # ELMo it is then
+            dset = self.data_file[f"{self.split_}/elmo/{index}"][...]
+            sentence_embeddings = dset[int(self.layer)]
+            if self.args["featurizer"]["decay"]:
+                sentence_embeddings = self.decay(sentence_embeddings, index)
+            elif self.projection:
+                with torch.no_grad():
+                    proj, _ = self.projection(torch.tensor(sentence_embeddings, device=args["device"]).unsqueeze(0))
+                sentence_embeddings = proj.squeeze(0)
+        return sentence_embeddings
+
+
+    def trees_sents_roots_lengths(self):
+        trees = []
+        sents = []
+        roots = []
+        lengths = []
+        with open(self.path_to_conllu, encoding="utf-8") as data_file:
+            for tokenlist in parse_incr(data_file):
+                tokens = [t["form"] for t in tokenlist if type(t["id"]) == int]
+                sents.append(tokens)
+                lengths.append(len(tokens))
+                G = nx.Graph()
+                G.add_edges_from([(t["id"], t["head"]) for t in tokenlist if (type(t["id"]) == int) and (t["head"] != 0)])
+                assert nx.is_tree(G)
+                trees.append(G)
+                root = [t["id"] for t in tokenlist if t["head"] == 0]
+                assert len(root) == 1
+                roots.append(root[0])
+        return trees, roots, sents, lengths
+
+    def plot(self, id):
+        nx.draw(self.trees[id], pos=nx.spring_layout(self.trees[id]), with_labels=True, font_weight='bold')
+
+    def __len__(self):
+        return len(self.sents)
+
+    def __getitem__(self, index):
+        return self.Sentence(tokens=self.sents[index], 
+                             length=self.lengths[index], 
+                             tree=self.trees[index],
+                             embeddings=self.read_embeddings(index))
+        
+    def wordpiece_tokenize(self):
+        subtokens = []
+        spans = []
+        for sent in tqdm(self.sents, desc="Wordpiece tokenizing:"):
+            sentence_spans = []
+            sentence_subtokens = []
+            for token in sent:
+                token_subtokens = self.tokenizer.wordpiece_tokenizer.tokenize(token)
+                sentence_spans.append((len(sentence_subtokens), len(sentence_subtokens) + len(token_subtokens)))
+                sentence_subtokens += token_subtokens
+            subtokens.append(sentence_subtokens)
+            spans.append(sentence_spans)
+        return subtokens, spans
+
+    def decay(self, embeddings, index):
+        decay = self.length_to_decay[ self.lengths[index] ]
+        dec = torch.matmul(decay, torch.tensor(embeddings, device=self.args["device"]).double())
+        return dec / self.length_to_norm[ self.lengths[index] ]
+
+    def compute_decays_and_norms(self):
+        decays = {}
+        norms = {}
+        for length in tqdm(set(self.lengths), desc="[computing decay]"):
+            range_ = torch.arange(length, device=self.args["device"])
+
+            distances = [torch.arange(0-i, length-i, device=self.args["device"]) for i in range_]
+            abs_distances_to_neighbours = torch.vstack(distances).abs()
+            # fp precision gotcha strikes if you use .pow instead of .float_power
+#             squared_abs_distances_to_neighbours = torch.float_power(2, abs_distances_to_neighbours)
+            squared_abs_distances_to_neighbours = torch.pow(2, abs_distances_to_neighbours.double())
+            decays.update({ length: torch.true_divide(1, squared_abs_distances_to_neighbours) })
+
+#             norm = 1 / torch.float_power(2, range_)
+            norm = 1 / torch.pow(2, range_.double())
+            
+            norm = torch.tensor([norm[:i+1].sum() for i in range_], device=self.args["device"])
+            norms.update({length: (norm + norm.flip([0])).unsqueeze(1)})
+        return decays, norms
+
+    def custom_pad(self, batch):
+        """Pads sequences with zero and labels with -1."""
+        seqs = [torch.tensor(x[0].embeddings, device=self.args["device"]) for x in batch]
+        lengths = torch.tensor([x[0].length for x in batch], device=self.args["device"])
+        padded_seqs = nn.utils.rnn.pad_sequence(seqs, batch_first=True)
+        label_shape = batch[0][1].shape
+        max_len = int(lengths.max())
+        labels_new_shape = [max_len for _ in label_shape]
+
+        padded_labels = [-torch.ones(*labels_new_shape, device=self.args["device"]) for _ in seqs]
+        for i, length in enumerate(lengths):
+            if len(label_shape) == 1:
+                padded_labels[i][:length] = batch[i][1]
+            elif len(label_shape) == 2:
+                padded_labels[i][:length, :length] = batch[i][1]
+            else:
+                raise ValueError("Labels must be either 1D or 2D.")
+        padded_labels = torch.stack(padded_labels)
+        return padded_seqs, padded_labels, lengths, batch
+    
+    def loader(self):
+        return torch.utils.data.DataLoader(self, batch_size=self.batch_size, collate_fn=self.custom_pad)
