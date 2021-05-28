6edef1a82a57ca69ea225f9f2a5ef716e5fd559b
lapythie <frbrmyau@gmail.com>
2021-05-28 11:33:09 +0300

    add trainer

--- demo.py
+++ demo.py
@@ -13,18 +13,19 @@ from transformers import BertTokenizer
 
 from utils.data import TwoWordDataset
 from utils.probe import TwoWordProbe
+from utils.training import ProbeTrainer, predict
 
 argp = ArgumentParser()
 argp.add_argument("--config_path", default=None, type=str, help="path to yaml config file")
 argp.add_argument("--conllu_dir", default=None, type=str, help="directory with train, dev and test parts of a conllu UD dataset")
 cli_args = argp.parse_args()
 
-path_to_config = "config/prd/str-prd-rubert-1.yaml"
+##path_to_config = "config/prd/str-prd-rubert-1.yaml"
 path_to_train = cli_args.conllu_dir+"/"+[p for p in os.listdir(cli_args.conllu_dir)
                                          if p.endswith(".conllu") and "train" in p][0]
 path_to_dev = cli_args.conllu_dir+"/"+[p for p in os.listdir(cli_args.conllu_dir)
                                        if p.endswith(".conllu") and "dev" in p][0]
-args = yaml.safe_load(open(path_to_config))
+args = yaml.safe_load(open(cli_args.config_path))
 
 np.random.seed(0)
 torch.manual_seed(0)
@@ -35,4 +36,4 @@ train = TwoWordDataset(args=args, path_to_conllu=path_to_train)
 dev = TwoWordDataset(args=args, path_to_conllu=path_to_dev)
 
 probe = TwoWordProbe(args)
-
+trainer = ProbeTrainer(args)
